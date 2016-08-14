ef6242dd433eded5b6ab332da7cbd41da54d6c3b
mwgoldsmith <mike.w.goldsmith@gmail.com>
2016-08-14 13:46:36 +0100

    Fix compiler compatibility issues in playlist/loadsave.c

--- src/playlist/loadsave.c
+++ src/playlist/loadsave.c
@@ -195,26 +195,28 @@ int playlist_MLDump( playlist_t *p_playlist )
         msg_Err( p_playlist, "no data directory, cannot save media library") ;
         return VLC_EGENERIC;
     }
-
-    char psz_dirname[ strlen( psz_temp ) + sizeof( DIR_SEP "ml.xspf")];
+    char *psz_dirname = (char *)malloc(strlen(psz_temp) + sizeof(DIR_SEP "ml.xspf"));
     strcpy( psz_dirname, psz_temp );
     free( psz_temp );
     if( config_CreateDir( (vlc_object_t *)p_playlist, psz_dirname ) )
     {
+        free(psz_dirname);
         return VLC_EGENERIC;
     }
 
     strcat( psz_dirname, DIR_SEP "ml.xspf" );
 
-    if ( asprintf( &psz_temp, "%s.tmp%"PRIu32, psz_dirname, (uint32_t)getpid() ) < 1 )
-        return VLC_EGENERIC;
-
+    if(asprintf(&psz_temp, "%s.tmp%"PRIu32, psz_dirname, (uint32_t)getpid()) < 1) {
+      free(psz_dirname);
+      return VLC_EGENERIC;
+    }
     int i_ret = playlist_Export( p_playlist, psz_temp, p_playlist->p_media_library,
                      "export-xspf" );
     if ( i_ret != VLC_SUCCESS )
     {
         vlc_unlink( psz_temp );
         free( psz_temp );
+        free(psz_dirname);
         return i_ret;
     }
 
@@ -224,7 +226,9 @@ int playlist_MLDump( playlist_t *p_playlist )
     {
         msg_Err( p_playlist, "could not rename %s.tmp: %s",
                  psz_dirname, vlc_strerror_c(errno) );
+        free(psz_dirname);
         return VLC_EGENERIC;
     }
+    free(psz_dirname);
     return VLC_SUCCESS;
 }
